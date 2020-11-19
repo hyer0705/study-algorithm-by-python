610c7905ee2d2b9030ce51f39c55f4d086cafbd5
hyer0705 <hyer1205@gmail.com>
2020-11-19 20:56:43 +0900

    add comment

--- 02_Implementation/07_chicken_delivery.py
+++ 02_Implementation/07_chicken_delivery.py
@@ -6,7 +6,7 @@
 # link: https://www.acmicpc.net/problem/15686
 
 # 내가 직접 작성한 코드
-# 책에 있는 테스트만 맞음...
+# max 값을 1e9로 바꾸고 성공!
 
 import itertools
 
@@ -32,23 +32,31 @@ for row in range(len(city_data)):
             chicken_cnt += 1
             chicken_data.append((row, col))
 
+# 도시의 치킨 거리 계산하는 함수
 def calc_chicken_distance_sum(house_data, chicken_data):
+    # 도시의 치킨 거리를 저장하는 변수
     chicken_distance_sum = 0
+    # 집의 위치를 기준으로 치킨집 까지의 거리를 계산하여 가장 치킨 거리를 구하는 반복문
     for house in house_data:
-        chicken_distance = n
+        # 치킨 거리의 초기값은 1e9로 설정하여 최솟값을 구한다
+        chicken_distance = 1e9
+        # 치킨집의 위치 만큼 반복
         for chicken in chicken_data:
+            # 현재 구한 거리를 저장하는 변수
             curr_distance = abs(house[0] - chicken[0]) + abs(house[1] - chicken[1])
+            # 치킨 거리 계산
             chicken_distance = min(chicken_distance, curr_distance)
+        # 도시의 치킨 거리에 방금 구한 치킨 거리를 더한다
         chicken_distance_sum += chicken_distance
+    # 도시의 치킨 거리 반환
     return chicken_distance_sum
 
-# 치킨 집의 갯수와 m과 같다면 경우의 수는 1번이므로
-result = 2500
-if chicken_cnt == m:
-    result = calc_chicken_distance_sum(house_data=house_data, chicken_data=chicken_data)
-else:
-    for chickens in itertools.combinations(chicken_data, m):
-        result = min(result, calc_chicken_distance_sum(house_data=house_data, chicken_data=chickens))
+# result 는 도시의 치킨 거리 중 최솟값을 저장하는 변수로 1e9를 초기값으로 넣어준다
+result = 1e9
+# 파이썬의 조합 라이브러리를 사용하여 최대 m개의 치킨집의 조합을 구하고
+# 도시의 치킨 거리 중 최솟값을 구한다
+for chickens in itertools.combinations(chicken_data, m):
+    result = min(result, calc_chicken_distance_sum(house_data=house_data, chicken_data=chickens))
 
 # 폐업시키지 않을 치킨집을 최대 M개를 골랐을 때,
 # 도시의 치킨 거리(모든 집의 치킨 거리의 합)의 최솟값을 출력
